b74acec91a338fd40a11d0c080030d8c254a6115
wubeZ <85889061+wubeZ@users.noreply.github.com>
2022-03-16 13:50:11 +0300

    Time: 45 ms (43.85%), Space: 14 MB (34.13%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,23 +3,18 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        self.ans = -1
-        
-        def finder(n, check):
-            left, right = 1, n
-            
-            while left <= right:
-                mid = left + (right - left)//2
+        left, right = 1, n
+        first = 0
+        while left <= right:
+            mid = left + (right - left)//2
                 
-                if not isBadVersion(mid):
-                    left = mid + 1
-                else:
-                    self.ans = mid
-                    if check:
-                        right = mid - 1
-                        
-            return self.ans
-        
-        first = finder(n, True)
+            if not isBadVersion(mid):
+                left = mid + 1
+            else:
+                first = mid
+                right = mid - 1
+            
         
         return first
+        
+        
