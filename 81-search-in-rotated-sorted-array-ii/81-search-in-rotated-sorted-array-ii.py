0ff62c65a8ecb97185c31e964f6a1b415e58e1ca
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-03-28 17:15:51 +0300

    Time: 99 ms (18.13%), Space: 14.4 MB (63.51%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -1,17 +1,58 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        nums.sort()
-        left = 0
-        right = len(nums) - 1
+        index = 0
+        for i in range(1,len(nums)):
+            if nums[i-1] > nums[i]:
+                index = i
+                break
         
-        while left <= right:
-            mid = left + (right - left)//2
+        def binarySearch(left, right):
+            while left <= right:
+                mid = left + (right - left)//2
+
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            return False
+        
+        return binarySearch(0, index-1) or binarySearch(index,len(nums)-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort()
+#         left = 0
+#         right = len(nums) - 1
+        
+#         while left <= right:
+#             mid = left + (right - left)//2
             
-            if nums[mid] == target:
-                return True
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+#             if nums[mid] == target:
+#                 return True
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
             
-        return False
+#         return False
