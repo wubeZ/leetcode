5fb9ce0d283833623ddc75a16d244b55940f27b6
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-03-28 16:13:17 +0300

    Time: 157 ms (10.08%), Space: 26.1 MB (69.71%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,18 +1,16 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        visited, cur = set(), ""
-        left, window = 0, 0
-        ans = []
+        visited, ans = set(), set()
+        left = 0
+        
         while left < len(s):
-            cur = ""
-            while (left+window) < len(s) and window < 10:
-                cur += s[left+window]
-                window += 1
+            cur = s[left: left+10]
+            
             if cur in visited:
-                ans.append(cur)
+                ans.add(cur)
+            
             visited.add(cur)
-            window = 0
             left += 1
         
-        return list(set(ans))
+        return list(ans)
         
