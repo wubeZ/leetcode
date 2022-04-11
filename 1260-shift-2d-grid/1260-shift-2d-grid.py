f006348630f1da8ddae88fc05f1adf9c070e5b78
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-04-11 14:23:35 +0300

    Time: 206 ms (64.90%), Space: 14.5 MB (13.09%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,34 +1,17 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        k = k %(len(grid) * len(grid[0]))
-        count = len(grid)*len(grid[0]) - k
-        ans = [[0]*len(grid[0]) for i in range(len(grid))]
-        temp, temp2 = deque(), deque()
-        
+        m,n = len(grid), len(grid[0])
+        k = k % (m*n)
         if k == 0:
             return grid
         
-        for i in range(len(grid)-1,-1,-1):
-            for j in range(len(grid[0])-1,-1,-1):
-                if k > 0:
-                    temp.append(grid[i][j])
-                    k -= 1
-                else:
-                    break
+        temp = [grid[i][j] for i in range(m) for j in range(n)]
+        temp = temp[-k:] + temp[:-k]
+        temp = deque(temp)
+        result = [[0]*n for i in range(m)]
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if count > 0:
-                    temp2.append(grid[i][j])
-                    count -= 1
-                else:
-                    break
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if len(temp) > 0:
-                    ans[i][j] = temp.pop()
-                elif len(temp2) > 0:
-                    ans[i][j] = temp2.popleft()
-        
-        return ans
+        for i in range(m):
+            for j in range(n):
+                result[i][j] = temp.popleft()
+                
+        return result
