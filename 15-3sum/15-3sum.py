04e56e00f245559a1c50ad50d6d84b8a96666910
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-04-12 12:44:04 +0300

    Time: 2861 ms (25.90%), Space: 174.9 MB (5.51%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,19 +3,19 @@ class Solution:
         res = []
         nums.sort()
         
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            left, right = i+1, len(nums)-1
+        for cur in range(len(nums)-2):
+            
+            left, right = cur + 1, len(nums)-1
             while left < right:
-                sums = nums[i] + nums[left] + nums[right]
+                sums = nums[cur] + nums[left] + nums[right]
                 if sums < 0:
                     left += 1 
                 elif sums > 0:
                     right -= 1
                 else:
-                    res.append((nums[i], nums[left], nums[right]))
+                    res.append((nums[cur], nums[left], nums[right]))
                     left += 1
                     right -= 1
+                    
         return list(set(res))
              
