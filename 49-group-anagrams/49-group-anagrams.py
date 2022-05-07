b8435489b95798995bb4d03fc060dce166821c4f
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-05-07 23:14:39 +0300

    Time: 99 ms (92.53%), Space: 18.4 MB (38.47%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        
+        
+        return ans.values()
