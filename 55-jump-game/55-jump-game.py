b27f9dd5f37860fb18161af68e459554b3730899
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-07-30 18:17:36 +0300

    Time: 684 ms (63.22%), Space: 15.2 MB (82.00%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        steps = 0
+        for i in range(len(nums)-1):
+            if nums[i] == 0 and steps == 0:
+                return False
+            steps = max(nums[i], steps)
+            steps -= 1
+        
+        return True
+        
+            
