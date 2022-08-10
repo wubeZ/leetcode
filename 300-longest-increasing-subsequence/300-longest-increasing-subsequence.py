a42746c64c7c14fe075b17f9d6137a1165805baf
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-10 17:10:05 +0300

    Time: 7861 ms (5.13%), Space: 14.3 MB (13.84%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         memo = [1] * len(nums)
-        
+        ans = -1
         for i in range(len(nums)-1,-1,-1):
             j = i + 1
             temp = 0
@@ -9,11 +9,9 @@ class Solution:
                 if nums[i] < nums[j]:
                     temp = max(memo[j], temp)
                 j += 1
-            memo[i] += temp 
-        
-        
-        ans = 0
-        for i in range(len(memo)):
+            memo[i] += temp
+            
             ans = max(ans, memo[i])
+        
         
         return ans
