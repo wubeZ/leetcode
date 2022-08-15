4a18b753e324b5bc7bb8527e85d203560124f307
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-15 10:51:09 +0300

    Time: 51 ms (55.41%), Space: 13.7 MB (97.45%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+        
+        return nums
+        
