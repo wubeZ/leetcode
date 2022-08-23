580d36bb67e9502d818c8445e849d524bac87e5a
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-23 16:05:50 +0300

    Time: 1831 ms (59.18%), Space: 47.3 MB (63.58%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graph = defaultdict(set)
+        adjacency = defaultdict(list)
+        
+        for x, y in connections:
+            graph[x].add(y)
+            adjacency[x].append(y)
+            adjacency[y].append(x)
+        
+        queue = deque()
+        res = 0
+        queue.append((0, 0))
+        
+        while queue:
+            
+            cur, parent = queue.popleft()
+            
+            for child in adjacency[cur]:
+                if child == parent:
+                    continue
+                if cur not in graph[child]:
+                    res += 1
+                        
+                queue.append((child, cur))
+        
+        return res
+            
