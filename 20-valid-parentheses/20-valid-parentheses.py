42edcbf9e86a259a228e18212709aa4b22477525
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-24 00:44:32 +0300

    Time: 32 ms (92.95%), Space: 13.8 MB (71.96%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        d = {"(":")", "{":"}", "[":"]"}
+        
+        for bracket in s:
+            if bracket in d:
+                stack.append(bracket)
+            else:
+                if stack and stack[-1] in d and d[stack[-1]] == bracket:
+                    stack.pop()
+                else:
+                    stack.append(bracket)
+        
+        return False if stack else True
+            
