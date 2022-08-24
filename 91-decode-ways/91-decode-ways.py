5845ff69786b16dfa38ea49fc50811ec6c457276
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-25 01:07:39 +0300

    Time: 65 ms (17.91%), Space: 14 MB (30.98%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,16 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        memo = {len(s): 1}
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
         
-        def dfs(i):
-            if i in memo : return memo[i]
-            if s[i] == "0": return 0
-            
-            res = dfs(i + 1)
-            if (i + 1 < len(s)) and ((s[i] == "1") or (s[i]=="2" and s[i+1] in "0123456")):
-                res += dfs(i + 2)
-            
-            memo[i] = res                       
-            return memo[i]
-    
-        return dfs(0)
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "0": continue
+            dp[i] = dp[i+1]
+            if i+1 < len(s) and ((s[i] == "1") or (s[i]=="2" and s[i+1] in "0123456")):
+                dp[i] += dp[i+2]
+        
+        return dp[0]
