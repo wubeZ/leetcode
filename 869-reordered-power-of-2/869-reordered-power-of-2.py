e2fb44fadbe0a349bd36ff8b4c48609c82fecf66
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-08-26 21:20:23 +0300

    Time: 78 ms (32.09%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,13 +1,3 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        num = str(n)
-        
-        perm = permutations(num)
-        
-        for p in list(perm):
-            if int(p[0]) != 0:
-                val = int("".join(p))
-                if bin(val).count("1") == 1:
-                    return True
-        
-        return False
+        return sorted(str(n)) in [sorted(str(1 << i)) for i in range(30)]
