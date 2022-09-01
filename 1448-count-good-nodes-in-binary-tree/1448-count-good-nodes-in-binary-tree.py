1dcc4f005cd8db964f8c6efb966d3933fa89606e
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-09-01 21:30:14 +0300

    Time: 561 ms (6.15%), Space: 32.6 MB (46.30%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -9,21 +9,12 @@ class Solution:
         self.count = 0
         
         def dfs(node, ref):
-            if not node:
-                return
-            
             if node.val >= ref:
                 self.count += 1
-                
-            if node.left and node.left.val >= ref:
-                dfs(node.left, node.left.val)
-            else:
-                dfs(node.left, ref)
-                
-            if node.right and node.right.val >= ref:
-                dfs(node.right, node.right.val)
-            else:
-                dfs(node.right, ref)
+            if node.left:
+                dfs(node.left, max(ref, node.left.val))
+            if node.right:
+                dfs(node.right, max(ref, node.right.val))
             
         dfs(root, root.val)
         return self.count
