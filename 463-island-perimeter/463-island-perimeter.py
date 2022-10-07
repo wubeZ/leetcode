54a56f4a8f00911e9d5ea4680c92347c71daf6ae
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-10-07 09:55:03 +0300

    Time: 1900 ms (5.33%), Space: 14.2 MB (72.31%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -3,25 +3,16 @@ class Solution:
         direction = [[0,1],[1,0],[-1,0],[0,-1]]
         in_bound = lambda x, y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
         
-        def dfs(r, c):
-        
-            for i, j in direction:
-                nr, nc = r + i, c + j
-                if not in_bound(nr,nc) or grid[nr][nc] == 0:
-                    self.count += 1
-                if in_bound(nr,nc) and (nr, nc) not in visited and grid[nr][nc] == 1:
-                    visited.add((nr,nc))
-                    dfs(nr, nc)
-        
-        
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    self.count = 0
-                    visited = {(i,j)}
-                    dfs(i,j)
-                    return self.count
-        
+        ans = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    count = 0
+                    for dx, dy in direction:
+                        nr, nc = r + dx, c + dy
+                        if in_bound(nr,nc) and grid[nr][nc] == 1:
+                            count += 1
+                    ans += (4 - count)
+        return ans
         
         
