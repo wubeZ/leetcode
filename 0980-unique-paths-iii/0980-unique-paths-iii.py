dfbd1febbd3f379369fb4421224e316619d54186
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-11-02 16:28:05 +0300

    Time: 160 ms (29.85%), Space: 13.8 MB (94.95%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,18 +1,15 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         directions = [[-1,0],[0,-1],[1,0],[0,1]]
-        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
-        count = 0
+        n, m = len(grid), len(grid[0])
+        inbound = lambda x,y: 0 <= x < n and 0 <= y < m
         self.ans = 0
-        nums = {}
-        total = (1 << len(grid[0])*len(grid)) - 1
+        total = (1 << n*m) - 1
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                nums[(i,j)] = count
-                count += 1
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == -1:
-                    total ^= 1 << nums[(i,j)] 
+                    total ^= 1 << ((i*m+j))
                 elif grid[i][j] == 1:
                     start = (i,j)
         
@@ -21,14 +18,13 @@ class Solution:
                 if grid[r][c] == 2:
                     self.ans += 1
                 return 
-            
             if grid[r][c] == 2:
                 return
             
             for x, y in directions:
                 nr,nc = r+x, c+y
-                if inbound(nr,nc) and grid[nr][nc] != -1 and (mask & (1 << nums[(nr,nc)]))== 0:
-                    dfs(nr,nc, mask | (1<< nums[(nr,nc)]))
+                if inbound(nr,nc) and grid[nr][nc] != -1 and mask & (1 << (nr*m + nc))== 0:
+                    dfs(nr,nc, mask | (1 << (nr*m + nc)))
                                         
-        dfs(start[0], start[1], 1 << nums[start])
+        dfs(start[0], start[1], 1 << (start[0]*m +start[1]))
         return self.ans
