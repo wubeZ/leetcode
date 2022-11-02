3ce638ac87de0c73f0a859f366fa65110e6bb7f1
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-11-02 16:05:13 +0300

    Time: 142 ms (38.41%), Space: 13.9 MB (57.53%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -2,18 +2,22 @@ class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         directions = [[-1,0],[0,-1],[1,0],[0,1]]
         inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
-        seen = set()
+        count = 0
         self.ans = 0
-        total = len(grid[0])*len(grid)
+        nums = {}
+        total = (1 << len(grid[0])*len(grid)) - 1
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
+                nums[(i,j)] = count
+                count += 1
                 if grid[i][j] == -1:
-                    total -= 1
+                    total ^= 1 << nums[(i,j)] 
                 elif grid[i][j] == 1:
                     start = (i,j)
         
-        def dfs(r,c):
-            if len(seen) == total:
+        def dfs(r,c, mask):
+            if mask == total:
                 if grid[r][c] == 2:
                     self.ans += 1
                 return 
@@ -23,10 +27,8 @@ class Solution:
             
             for x, y in directions:
                 nr,nc = r+x, c+y
-                if inbound(nr,nc) and grid[nr][nc] != -1 and (nr,nc) not in seen:
-                    seen.add((nr,nc))
-                    dfs(nr,nc)
-                    seen.remove((nr,nc))
-        seen.add(start)                    
-        dfs(start[0], start[1])
+                if inbound(nr,nc) and grid[nr][nc] != -1 and (mask & (1 << nums[(nr,nc)]))== 0:
+                    dfs(nr,nc, mask | (1<< nums[(nr,nc)]))
+                                        
+        dfs(start[0], start[1], 1 << nums[start])
         return self.ans
