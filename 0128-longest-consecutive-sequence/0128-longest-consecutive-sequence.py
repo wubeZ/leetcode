edbfb6e0ee05728b8cc782539a49208f799776bb
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-11-18 22:34:20 +0300

    Time: 2594 ms (35.90%), Space: 28.9 MB (85.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,34 +1,17 @@
 class Solution:
-    
-    def find(self, x):
-        if x == self.parent[x]:
-            return x
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        px = self.find(x)
-        py = self.find(y)
-        
-        if px != py:
-            if self.size[px] >= self.size[py]:
-                self.parent[py] = px 
-                self.size[px] += self.size[py] 
-            else:
-                self.parent[px] = py
-                self.size[py] += self.size[px]
-    
-    
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0: return 0
-        self.parent = {nums[i]:nums[i] for i in range(len(nums))}
-        self.size = {nums[i]:1 for i in range(len(nums))}
+        ans = 1
+        numset = set(nums)
         
         for i in range(len(nums)):
-            if nums[i] in self.parent and (nums[i]+1) in self.parent:
-                self.union(nums[i], nums[i]+1)
-            if nums[i] in self.parent and (nums[i]-1) in self.parent:
-                self.union(nums[i], nums[i]-1)
-            
-        return max(self.size.values())
+            if nums[i]-1 not in numset:
+                count = 1
+                num = nums[i]
+                while num + 1 in numset:
+                    count += 1
+                    num += 1
+                ans = max(ans, count)
+        
+        return ans
             
