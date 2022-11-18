c703bc6b0a248238146453aafd8a48fd82cfaa4b
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-11-18 16:44:52 +0300

    Time: 84 ms (72.30%), Space: 14.1 MB (16.12%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -3,10 +3,14 @@ class Solution:
         counter1 = Counter(nums1)
         counter2 = Counter(nums2)
         ans = []
+        if len(counter1) >= len(counter2):
+            keys = counter2.keys()
+        else:
+            keys = counter1.keys()
         
-        for i in counter1:
-            if i in counter2:
-                for _ in range(min(counter1[i], counter2[i])):
-                    ans.append(i)
+        for k in keys:
+            if k in counter2 and k in counter1:
+                for _ in range(min(counter1[k], counter2[k])):
+                    ans.append(k)
     
         return ans
