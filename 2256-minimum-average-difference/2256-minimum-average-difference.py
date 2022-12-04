48109640b8782fc3f585fc3d5e60ed6cfb697f5b
Wubshet Zeleke <85889061+wubeZ@users.noreply.github.com>
2022-12-04 11:12:44 +0300

    Time: 2858 ms (25.74%), Space: 24.8 MB (80.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,21 +1,15 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        frontprefix = [0]
-        for i in range(len(nums)):
-            frontprefix.append(nums[i]+frontprefix[-1])
-        backprefix = [0]
-        for i in range(len(nums)-1,-1,-1):
-            backprefix.append(nums[i]+backprefix[-1])
+        total = sum(nums)
         
-        backprefix = backprefix[::-1]
-        frontprefix = frontprefix[1:]
-
         ans = 0
         maxval = float("inf")
-
+        left = 0
         for i in range(len(nums)):
-            x = frontprefix[i]//(i+1) if frontprefix[i] != 0 else 0 
-            y = backprefix[i+1]//(len(nums)-i-1) if backprefix[i+1] != 0 else 0
+            left += nums[i]
+            right = total - left
+            x = left//(i+1) if left != 0 else 0 
+            y = right//(len(nums)-i-1) if right != 0 else 0
             val = abs(x-y)
             if maxval > val:
                 maxval = val
