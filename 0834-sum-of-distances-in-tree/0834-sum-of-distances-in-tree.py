55b3a2378196470a0102346d7c3c4218f6bfd995
Wubshet Zeleke <wubezeleke@gmail.com>
2022-12-23 11:51:05 +0300

    Time: 1560 ms (70.97%), Space: 65.6 MB (38.71%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -1,55 +1,30 @@
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        
-        for src, des in edges:
-            graph[src].append(des)
-            graph[des].append(src)
-            
-        visited = set([0])
         memo = {}
-        ans = [0] * n
+        answer = [0 for i in range(n)]
+        for src, dest in edges:
+            graph[src].append(dest)
+            graph[dest].append(src)
         
-        def calculate(node):
+        def first_dfs(num, parent):
+            val1, val2 = 0, 0
             
-            node_size, node_dis = 0, 0
-            
-            for childnode in graph[node]:
-                if childnode not in visited:
-                    visited.add(childnode)
-                    new_dis, new_size = calculate(childnode)
-                    node_size += new_size
-                    node_dis += new_dis
+            for nei in graph[num]:
+                if nei != parent:
+                    x, y = first_dfs(nei, num)
+                    val1 += x + y
+                    val2 += y
+            memo[num] = [val1, val2 + 1]   
+            return memo[num]
+        
+        answer[0] = first_dfs(0, -1)[0]
+        
+        def get_result(num, parent):
+            for nei in graph[num]:
+                if nei != parent:
+                    answer[nei] = answer[num] + n - (2 * memo[nei][1])
+                    get_result(nei, num)
                     
-            memo[node] = (node_dis + node_size, node_size)
-            
-            return (node_dis + node_size, node_size + 1)
-        
-        def find(node):
-            
-            for childnode in graph[node]:
-                if childnode not in visited:
-                    par_dis, par_size = memo[node]
-                    child_dis, child_size = memo[childnode]
-                    value = par_dis - child_size - 1
-                    value += n - (child_size + 1)
-                    ans[childnode] = value
-                    memo[childnode] = (value, child_size)
-                    visited.add(childnode)
-                    find(childnode)
-            
-        calculate(0)
-        
-        visited = set([0])
-        
-        find(0)
-        ans[0] = memo[0][0]
-        return ans
-            
-            
-            
-            
-            
-            
-        
-        
+        get_result(0, 0)
+        return answer
