d5b9bafc65442a7aecbd7395ac33b18e1994ca43
Wubshet Zeleke <wubezeleke@gmail.com>
2022-12-28 12:33:55 +0300

    Time: 2025 ms (73.24%), Space: 28.6 MB (67.75%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -8,12 +8,8 @@ class Solution:
             temp = math.ceil(val / 2)
             heapq.heappush(heap, -temp)
             k -= 1
-        
-        ans = 0
-        for num in heap:
-            ans += abs(num)
             
-        return ans
+        return -sum(heap)
         
         
             
