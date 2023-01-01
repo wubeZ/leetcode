6f2a4187eee7686b09578da5f9bba191258a57fb
Wubshet Zeleke <wubezeleke@gmail.com>
2023-01-01 20:36:29 +0300

    Time: 33 ms (81.31%), Space: 13.9 MB (22.42%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,26 +1,15 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         arr = list(s.split())
-        if len(arr) != len(pattern):
+        if (len(arr) != len(pattern)) or (len(set(pattern)) != len(set(arr))):
             return False
         
-        pattern_map = {}
-        s_map = {}
-        
-        for i in range((len(pattern))):
-            pattern_map[pattern[i]] = arr[i]
-            s_map[arr[i]] = pattern[i]
-        
-        for key in s_map:
-            if pattern_map[s_map[key]] != key:
-                return False
-            
-        for key in pattern_map:
-            if s_map[pattern_map[key]] != key:
-                return False
+        maps = {}
         
         for i in range(len(pattern)):
-            if pattern_map[pattern[i]] != arr[i]:
+            if arr[i] not in maps:
+                maps[arr[i]] = pattern[i]
+            elif maps[arr[i]] != pattern[i]:
                 return False
         
         return True
