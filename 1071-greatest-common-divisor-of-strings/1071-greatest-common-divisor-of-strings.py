1a5dc25aa2338a5c2542c5a6a0f744db49ecaa10
Wubshet Zeleke <wubezeleke@gmail.com>
2023-02-01 21:27:02 +0300

    Time: 37 ms (64.59%), Space: 13.8 MB (98.52%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        idx = math.gcd(len(str1), len(str2))
+        
+        return str1[:idx]
+        
+        
