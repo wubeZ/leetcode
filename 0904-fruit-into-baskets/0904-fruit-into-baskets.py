0db4d751c974bafb61f5b3827ec63031cd21f0d5
Wubshet Zeleke <wubezeleke@gmail.com>
2023-02-07 19:13:23 +0300

    Time: 961 ms (73.68%), Space: 20.5 MB (28.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,21 +1,26 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left , window , maxfruit = 0, 0, 0
-        d = {}
+        max_size = 1
         
-        while left < len(fruits):
-            if fruits[left] not in d:
-                d[fruits[left]] = 1
-            else:
-                d[fruits[left]] += 1      
+        myset = set()
+        l = 0
+        last_postion = {}
+        
+        for r in range(len(fruits)):
+            myset.add(fruits[r])
+            last_postion[fruits[r]] = r
             
-            while len(d) > 2:
-                d[fruits[window]] -= 1
-                if d[fruits[window]] == 0:
-                    del d[fruits[window]]
-                window += 1
+            while len(myset) > 2:
+                temp, k = float("inf"), 0
+                for key in last_postion:
+                    if temp > last_postion[key]:
+                        temp = last_postion[key]
+                        k = key
+                        
+                del last_postion[k]
+                myset.remove(fruits[temp])
+                l = temp + 1
                 
-            maxfruit = max(maxfruit, left-window +1)
-            left += 1
+            max_size = max(max_size, r - l + 1)
             
-        return maxfruit
+        return max_size
