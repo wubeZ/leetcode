818f6b37dd79edfe4ebd983b7a2b9dce090db5b0
Wubshet Zeleke <wubezeleke@gmail.com>
2023-02-07 11:37:58 +0300

    Time: 47 ms (76.51%), Space: 14.2 MB (30.80%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,15 +1,15 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        stack = []
+        stack = 0
         
         for i in range(len(logs)):
             if logs[i] == "./":
                 continue
             elif logs[i] == "../":
-                if stack:
-                    stack.pop()
+                if stack > 0:
+                    stack -= 1
             else:
-                stack.append(logs[i])
+                stack += 1
         
         
-        return len(stack)
+        return stack
