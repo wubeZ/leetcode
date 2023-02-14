c6c96a0a1b9f3d2e62d3307b8c81829f979eb659
Wubshet Zeleke <wubezeleke@gmail.com>
2023-02-14 17:24:39 +0300

    Time: 30 ms (92.65%), Space: 14.5 MB (9.16%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,16 +1,15 @@
 class Solution:
+    
+    def solve(self, arr, k, idx):
+        if len(arr) == 1:
+            return arr[0]
+        
+        last_idx = (idx + k - 1) % len(arr)
+        arr.pop(last_idx)
+        
+        return self.solve(arr, k , last_idx)
+    
     def findTheWinner(self, n: int, k: int) -> int:
         arr = [i for i in range(1, n+1)]
         
-        def find(idx , k):
-            if len(arr) == 1:
-                return arr[0]
-            
-            last_idx = (idx + k - 1) % len(arr)
-            
-            arr.pop(last_idx)
-            
-            return find(last_idx, k)  
-        
-        
-        return find(0,k)
+        return self.solve(arr, k , 0)
