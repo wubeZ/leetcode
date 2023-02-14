da8794927fad970e8bcdc6580ea9ff8b04c5462d
Wubshet Zeleke <wubezeleke@gmail.com>
2023-02-14 12:16:06 +0300

    Time: 253 ms (91.39%), Space: 15.3 MB (57.62%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,13 +1,14 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         ans = 0
-        b_stack = []
+        b_count = 0
         
         for ch in s:
             if ch == "b":
-                b_stack.append(ch)
-            elif b_stack:
-                b_stack.pop()
+                b_count += 1
+                
+            elif b_count > 0:
+                b_count -= 1
                 ans += 1
         
         return ans
