47cf8fcfbde584d2c3e6d5be6e60402ae719e1bf
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-05 13:56:04 +0300

    Time: 87 ms (72.15%), Space: 15.4 MB (88.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,53 +1,31 @@
 class Solution:
     
-    def searchLeft(self, nums, target):
+    def search(self, nums, target, turn):
         left = -1
         right = len(nums)
         
         while left + 1 < right:
-            
             mid = left + (right - left)//2
             
             if nums[mid] < target:
                 left = mid
+            elif nums[mid] == target:
+                if turn:
+                    right = mid
+                else:
+                    left = mid
             else:
                 right = mid
         
-        return right if right != len(nums) and nums[right] == target else -1
-        
-        
-        
-        
-    def searchRight(self, nums, target):
-        left = -1
-        right = len(nums)
-        
-        while left + 1 < right:
-            
-            mid = left + (right - left)//2
-            
-            if nums[mid] <= target:
-                left = mid
-            else:
-                right = mid
+        if turn:
+            return right if right != len(nums) and nums[right] == target else -1
         
         return left if nums[left] == target else -1
         
-    
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1,-1]
-        left = self.searchLeft(nums, target)
-        right = self.searchRight(nums, target)
+        left = self.search(nums, target, True)
+        right = self.search(nums, target, False)
         
         return [left, right]
-        
-        
-        
-        
-        
-        
-    
-        
-            
-            
