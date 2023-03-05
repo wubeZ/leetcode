e11b960ffa05eef8bac0f09cb9ccb1badcb138b8
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-05 10:06:55 +0300

    Time: 1114 ms (91.52%), Space: 30.4 MB (43.98%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,26 +1,19 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        prefix = [0]
         n = len(grid[0])
-        suffix = [0]
         
-        for i in range(n):
-            prefix.append(prefix[-1] + grid[0][i])
-        
-        for j in range(n-1,-1,-1):
-            suffix.append(suffix[-1] + grid[1][j])
-        
-        prefix = prefix[1:]
-        suffix = suffix[::-1]
-        suffix.pop()
+        left = 0
+        right = sum(grid[0])
         
         ans = float("inf")
         
         for i in range(n):
-            left = prefix[-1] - prefix[i]
-            right = suffix[0] - suffix[i]
+            right -= grid[0][i]
             
             possible = max(left, right)
-            ans = min(ans , possible)
+            ans = min(ans, possible)
+            
+            left += grid[1][i]
+        
         
         return ans
