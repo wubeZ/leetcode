b5c1d5af737fc7efbb6ae6455c287038cfe16817
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-05 09:05:48 +0300

    Time: 468 ms (77.80%), Space: 15.4 MB (97.79%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+    def checker(self, mid, piles, h):
+        hours = 0
+        for num in piles:
+            hours += math.ceil(num / mid)
+
+        return hours <= h
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        left = 0
+        right = max(piles)
+        
+        while left + 1 < right:
+            
+            mid = left + (right - left)//2
+            
+            isValid = self.checker(mid, piles, h)
+            
+            if not isValid:
+                left = mid
+            else:
+                right = mid
+        
+        
+        return right
+                
