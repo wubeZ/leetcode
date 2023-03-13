6c1485123f4ae0af3497c2c08ae029285157e339
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-14 00:28:15 +0300

    Time: 635 ms (95.91%), Space: 28.9 MB (95.00%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,31 +1,20 @@
 class Solution:
+    
+    def backtrack(self, index, arr, s):
+        if index >= len(s):
+            self.ans.append(arr[:])
+            return 
+        
+        for i  in range(index, len(s)):
+            val = s[index : i + 1]
+            if val == val[::-1]:
+                self.backtrack(i + 1, arr + [val], s)
+            
+    
     def partition(self, s: str) -> List[List[str]]:
+        self.ans = []
         
-        def palindrome_check(l,r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
+        self.backtrack(0, [], s)
         
-        def backtrack(cur_idx, past_idx):
-            if cur_idx == len(s):
-                if path and past_idx == cur_idx:
-                    ans.append(path[:])
-                return 
-            isvalid = palindrome_check(past_idx, cur_idx)
-            
-            if isvalid:
-                path.append(s[past_idx:cur_idx+1])
-                backtrack(cur_idx+1, cur_idx+1)
-                path.pop()
-            
-            backtrack(cur_idx+1, past_idx)
-            
-
-        ans = []
-        path = []
-        backtrack(0, 0)
-        
-        return ans
+        return self.ans
+       
