b99783d429f120d80f2552883489ce6d8b69b1a0
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-14 00:49:54 +0300

    Time: 167 ms (20.53%), Space: 13.8 MB (98.95%) - LeetHub

--- 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.py
+++ 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.py
@@ -7,21 +7,21 @@ class Solution:
         
     def backtrack(self, index, arr, num):
         if index >= len(num) and len(arr) >= 3:
-            self.ans = list(map(int, arr))
-            return True
+            return (True, list(map(int, arr)) )
         
         for i in range(index, len(num)):
             val = num[index : i + 1]
             if self.check(val) and (len(arr) < 2 or ( int(val) == int(arr[-1]) + int(arr[-2]) ) ):
                 isValid = self.backtrack(i + 1, arr + [val], num)
-                if isValid:
-                    return True
+                if isValid[0]:
+                    return isValid
         
-        return False
+        return (False, [])
             
     
     def splitIntoFibonacci(self, num: str) -> List[int]:
-        self.ans = []                       
-        self.backtrack(0, [], num)
-        return self.ans
+        
+        result = self.backtrack(0, [], num)
+        
+        return result[1]
                                
