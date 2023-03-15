1971456bded0c1596857e7f9c402542dcdf8686d
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-15 23:37:01 +0300

    Time: 30 ms (94.55%), Space: 14 MB (19.08%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -7,35 +7,35 @@
 class Solution:
     
     def __init__(self):
-        self.ans = True
         self.max_height = 0
         self.min_height = 101
     
     def traverse(self, node, height):
         if not node:
-            return height
+            return (height, True)
         
         right = self.traverse(node.right, height + 1)
         
-        if self.max_height > right:
-            self.ans = False
+        
+        if not right[1] or self.max_height > right[0]:
+            return (right[0], False)
         
         left = self.traverse(node.left, height + 1)
         
-        if self.max_height > left:
-            self.ans = False
+        if not left[1] or self.max_height > left[0]:
+            return (left[0], False)
         
-        self.max_height = max(self.max_height, left, right)
-        self.min_height = min(self.min_height, left, right)
+        self.max_height = max(self.max_height, left[0], right[0])
+        self.min_height = min(self.min_height, left[0], right[0])
         
         if abs(self.max_height - self.min_height) > 1:
-            self.ans = False
+            return (left[0], False)
             
         return left
         
     
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
         
-        self.traverse(root, 0)
+        result = self.traverse(root, 0)
         
-        return self.ans
+        return result[1]
