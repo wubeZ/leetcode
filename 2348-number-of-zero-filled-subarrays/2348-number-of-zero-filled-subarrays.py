143199475ef78a330a4f88e99f7157728be6c908
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-21 08:05:46 +0300

    Time: 1151 ms (29.33%), Space: 24.5 MB (100.00%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -2,10 +2,6 @@ class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         ans = 0
         
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                ans += 1
-        
         i = 0
         while i < len(nums):
             
@@ -13,8 +9,7 @@ class Solution:
             count = 0
             while j < len(nums) and nums[j] == 0:
                 count += 1
-                if count > 1:
-                    ans += count - 1
+                ans += count
                 j += 1
                 
             i = j
