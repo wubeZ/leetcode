54ab3e59e7a3870ea6d15507fd5364284ae28e01
Wubshet Zeleke <wubezeleke@gmail.com>
2023-03-24 15:30:49 +0300

    Time: 1488 ms (19.71%), Space: 27.5 MB (35.35%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,30 +1,40 @@
 class Solution:
-    def partition(self, l, r, nums):
-        
-        pivot_element = nums[r]
-        
-        i = l - 1
-        for j in range(l, r):
-            if nums[j] <= pivot_element:
+    def merge(self, a, b):
+        c = []
+        len1 = len(a)
+        len2 = len(b)
+        i , j = 0, 0
+        while i < len1 and j < len2:
+            if a[i] < b[j]:
+                c.append(a[i])
                 i += 1
-                nums[i], nums[j] = nums[j], nums[i]
-            
-        nums[i + 1], nums[r] = nums[r], nums[i + 1]
+            else:
+                c.append(b[j])
+                j += 1
         
-        return i + 1
+        while i < len1:
+            c.append(a[i])
+            i += 1
         
-    def quicksort(self, l, r, nums):
+        while j < len2:
+            c.append(b[j])
+            j += 1
+        
+        return c
+        
+    def mergesort(self, l, r, nums):
         if l >= r:
-            return
+            return [nums[l]]
         
-        mid = self.partition(l, r , nums)
+        mid = l + (r - l)//2
         
-        leftHalf = self.quicksort(l, mid - 1, nums)
-        rightHalf = self.quicksort(mid + 1, r, nums)
+        leftHalf = self.mergesort(l, mid, nums)
+        rightHalf = self.mergesort(mid + 1, r, nums)
         
+        return self.merge(leftHalf, rightHalf)
     
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        self.quicksort(0, len(nums)-1, nums)
-        
-        return nums[-k]
+        arr = self.mergesort(0, len(nums)-1, nums)
+    
+        return arr[-k]
