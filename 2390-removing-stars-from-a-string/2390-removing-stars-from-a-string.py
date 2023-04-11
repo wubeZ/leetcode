511968f71e398d7ca847d01e9145b3e9b3090c9b
Wubshet Zeleke <wubezeleke@gmail.com>
2023-04-11 07:46:25 +0300

    Time: 348 ms (21.89%), Space: 15.5 MB (77.42%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        
+        for i in range(len(s)):
+            if stack and s[i] == "*":
+                stack.pop()
+            else:
+                stack.append(s[i])
+                
+        return "".join(stack)
