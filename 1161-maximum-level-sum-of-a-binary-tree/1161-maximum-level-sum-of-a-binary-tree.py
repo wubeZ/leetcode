7201b24fe77c4b9a9c5b0c8babeda8e36334134f
Wubshet Zeleke <wubezeleke@gmail.com>
2023-05-07 21:19:56 +0300

    Time: 315 ms (39.10%), Space: 21.1 MB (5.45%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -9,11 +9,16 @@ class Solution:
         
         queue = deque()
         queue.append((root, 1))
-        total_sum = root.val
-        ans = 1
+        total_sum = float("-inf")
+        ans = 0
         
         
         while queue:
+            sum_nodes = sum([node.val for node, level in queue])
+            if total_sum < sum_nodes:
+                total_sum = sum_nodes
+                ans = queue[0][1]
+            
             n = len(queue)
             for i in range(n):
                 cur, level = queue.popleft()
@@ -22,12 +27,6 @@ class Solution:
                     queue.append((cur.left, level + 1))
                 if cur.right:
                     queue.append((cur.right, level + 1))
-            if not queue:
-                break
-            sum_nodes = sum([node.val for node, level in queue])
-            if total_sum < sum_nodes:
-                total_sum = sum_nodes
-                ans = level + 1
         
         return ans
         
