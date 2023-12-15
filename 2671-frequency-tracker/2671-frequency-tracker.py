2aec4c364b1e3ddcd9054062e3722d4d4d09f2a5
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2023-12-15 23:32:20 +0300

    Time: 543 ms (34.25%), Space: 72.5 MB (60.24%) - LeetHub

--- 2671-frequency-tracker/2671-frequency-tracker.py
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -5,31 +5,21 @@ class FrequencyTracker:
         self.freqFreq = defaultdict(int)
 
     def add(self, number: int) -> None:
-        if number in self.freq:
-            self.freqFreq[self.freq[number]] -= 1
-            if self.freqFreq[self.freq[number]] == 0:
-                del self.freqFreq[self.freq[number]]
-            self.freq[number] += 1
-            self.freqFreq[self.freq[number]] += 1 
-        else:
-            self.freq[number] += 1
-            self.freqFreq[self.freq[number]] += 1
+        self.freqFreq[self.freq[number]] = max(0, self.freqFreq[self.freq[number]] - 1)
+        self.freq[number] += 1
+        self.freqFreq[self.freq[number]] += 1
 
     def deleteOne(self, number: int) -> None:
-        if number in self.freq:
-            self.freqFreq[self.freq[number]] -= 1
-            if self.freqFreq[self.freq[number]] == 0:
-                del self.freqFreq[self.freq[number]]
-            self.freq[number] -= 1
-            if self.freq[number] == 0:
-                del self.freq[number]
-            else:
-                self.freqFreq[self.freq[number]] += 1
+        self.freqFreq[self.freq[number]] = max(0, self.freqFreq[self.freq[number]] - 1)
+        self.freq[number] = max(0, self.freq[number] - 1)
+        if self.freq[number] == 0:
+            del self.freq[number]
+        else:
+            self.freqFreq[self.freq[number]] += 1
 
     def hasFrequency(self, frequency: int) -> bool:
-        if frequency in self.freqFreq:
-            return True
-        return False
+        
+        return self.freqFreq[frequency] > 0
 
 
 # Your FrequencyTracker object will be instantiated and called as such:
