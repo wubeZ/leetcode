ef61d717ab30635d6588a53f8da8181fc737f5d9
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2024-01-04 20:07:04 +0300

    Time: 350 ms (95.03%), Space: 24.1 MB (19.24%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -7,18 +7,14 @@ class Solution:
         
         count = 0
         
-        while left < right:
+        while left <= right:
             if people[left] + people[right] <= limit:
-                count += 1
                 left += 1
                 right -= 1
             else:
                 left += 1
-                count += 1
             
-            if left == right:
-                count += 1
-        
+            count += 1
         
         return count
         
