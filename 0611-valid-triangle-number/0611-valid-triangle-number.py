f693412a15895b8ef1b5b8ec564b6d6546b9d785
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2024-02-21 15:48:09 +0300

    Time: 478 ms (78.04%), Space: 16.7 MB (82.98%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -7,9 +7,9 @@ class Solution:
         for i in range(len(nums)-1,-1,-1):
             l = 0
             r = i - 1
-            curr = nums[i]
+            
             while l < r:
-                if nums[l] + nums[r] > curr:
+                if nums[l] + nums[r] > nums[i]:
                     ans += r - l
                     r -= 1
                 else:
