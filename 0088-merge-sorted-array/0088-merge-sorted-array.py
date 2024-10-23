4273f08d031d1e255e753906b3632dfbc60ad83f
Wubshet Zeleke <wubezeleke@gmail.com>
2024-10-24 00:17:34 +0300

    Time: 0 ms (100.00%), Space: 16.7 MB (25.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,21 +5,14 @@ class Solution:
         ptr3 = len(nums1) - 1
         
         
-        while (ptr1 > -1 and ptr2 > -1) or (ptr1 < 0 and ptr2 > -1):
+        while ptr2 > -1:
             if ptr1 > -1 and nums1[ptr1] >= nums2[ptr2]:
                 nums1[ptr3] = nums1[ptr1]
-                ptr3 -= 1
                 ptr1 -= 1
             else:
                 nums1[ptr3] = nums2[ptr2]
-                ptr3 -= 1
                 ptr2 -= 1
-        
-        
-        # while ptr2 > -1:
-        #     nums1[ptr3] = nums2[ptr2]
-        #     ptr2 -= 1
-        #     ptr3 -= 1
+            ptr3 -= 1
         
         
         
