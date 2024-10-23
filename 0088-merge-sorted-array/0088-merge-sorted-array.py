402d80e49dcf444b32bb2754ffb68333c5485297
Wubshet Zeleke <wubezeleke@gmail.com>
2024-10-24 00:12:32 +0300

    Time: 0 ms (100.00%), Space: 16.7 MB (25.41%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        ptr1 = len(nums1) - len(nums2) - 1
+        ptr2 = len(nums2) - 1
+        ptr3 = len(nums1) - 1
+        
+        
+        while ptr1 > -1 and ptr2 > -1:
+            if nums1[ptr1] >= nums2[ptr2]:
+                nums1[ptr3] = nums1[ptr1]
+                ptr3 -= 1
+                ptr1 -= 1
+            else:
+                nums1[ptr3] = nums2[ptr2]
+                ptr3 -= 1
+                ptr2 -= 1
+        
+        
+        while ptr2 > -1:
+            nums1[ptr3] = nums2[ptr2]
+            ptr2 -= 1
+            ptr3 -= 1
+        
+        
+        
+                
+        
