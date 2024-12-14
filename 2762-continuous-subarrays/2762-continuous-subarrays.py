77c9202cd9a934c49210561bf0020c5f4b115739
Wubshet Zeleke <wubezeleke@gmail.com>
2024-12-14 16:42:54 +0300

    Time: 685 ms (52.11%), Space: 27.6 MB (21.78%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.py
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -1,23 +1,24 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
-        counter = defaultdict(int)
+        ans = len(nums)
         
-        ans = 0
-        l, r = 0, 0
+        wind = {nums[0]: 1}
+        
+        l, r = 0, 1
         
         while r < len(nums):
-            counter[nums[r]] += 1
+            wind[nums[r]] = wind.get(nums[r], 0) + 1
             
-            while max(counter) - min(counter) > 2:
-                counter[nums[l]] -= 1
-                if counter[nums[l]] == 0:
-                    del counter[nums[l]]
-                
+            while (max(wind) - min(wind)) > 2:
+                wind[nums[l]] -= 1
+                if wind[nums[l]] == 0:
+                    del wind[nums[l]]
                 l += 1
-                
-            ans += r - l + 1
             
+            ans += r - l
+                
             r += 1
+                
         
         
         return ans
